ba95f8f34dacf20202759f5ac5c5de8fbcd82857
KevinDillaerts <69395404+KevinDillaerts@users.noreply.github.com>
2021-12-25 14:18:21 +0100

    added contact page, organised scss files based on type

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,15 @@
 import "../css/style.scss";
 
+const navToggle = document.querySelector(".nav__trigger");
+const menu = document.querySelector(".nav__wrapper");
+
+navToggle.onclick = () => {
+  navToggle.classList.toggle("nav__trigger--active");
+  menu.classList.toggle("nav__wrapper--active");
+};
+
 /* 
 To do's:
-- add menu functionality
 - add underline active link functionality
 - add favicons
 */
