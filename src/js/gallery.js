682d2143d6b33ac81a59f3ee771adc5872d8b53f
KevinDillaerts <69395404+KevinDillaerts@users.noreply.github.com>
2023-09-25 12:22:02 +0200

    added previous play

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -29,14 +29,11 @@ render();
 //   for (i = 1; i <= 28; i++) {
 //     const s = String(i);
 //     htmlstring += `
-//     <a href="../images/producties/bakkertje_pol/bakkertje_pol_${s.padStart(3, "0")}.jpg">
+//     <a href="../images/producties/onderweg/onderweg_${s.padStart(3, "0")}.jpg">
 //     <img
 //       class="gallery__thumbnail"
-//       src="../images/producties/bakkertje_pol/thumbnails/bakkertje_pol_thumb_${s.padStart(
-//         3,
-//         "0"
-//       )}.jpg"
-//       alt="Foto uit Bakkertje Pol"
+//       src="../images/producties/onderweg/thumbnails/onderweg_thumb_${s.padStart(3, "0")}.jpg"
+//       alt="Foto uit Onderweg"
 //     />
 //   </a>
 // `;
